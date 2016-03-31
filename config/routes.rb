c9b5782abd1a86e80809757e86cf7388c511add4
Amour <leeizamo@gmail.com>
2016-03-31 16:42:01 -0400

    added about and home pgs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
+  root 'pages#home' #This is our landing page
+  
+  get 'about' => 'pages#about' #creates about_path 
+
+
+
+
+
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
